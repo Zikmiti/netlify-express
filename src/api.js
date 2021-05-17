fcd3e8b9c723344c2b5aab3c5390a4656530815a
Zikmiti <41624992+Zikmiti@users.noreply.github.com>
2021-05-17 11:31:24 +1100

    init

---
+++ src/api.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const serverless = require("serverless-http");
+var request = require('request');
+
+const app = express();
+const router = express.Router();
+
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+router.get("/", (req, res) => {
+  var options = {
+    'method': 'GET',
+    'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
+   
+  };
+  options.url += 'cmarin03';
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.json(response.body)
+  });
+});
+
+
+router.get("/:uid", (req, res) => {
+  var options = {
+    'method': 'GET',
+    'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
+   
+  };
+  options.url += req.params.uid;
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.json(response.body)
+  });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
