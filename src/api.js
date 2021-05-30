4649c04aa4884ed6eaabd01a6958f8ff5198a891
Zikmiti <41624992+Zikmiti@users.noreply.github.com>
2021-05-31 09:56:03 +1100

    parse ical2json

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const ical2json = require("ical2json");
 var request = require('request');
+const http = require('http');
+const fs = require('fs');
+
 
 const app = express();
 const router = express.Router();
@@ -25,32 +29,69 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+
+
+
 router.get("/", (req, res) => {
+  
+var file = fs.createWriteStream("data.ics");
+
   var options = {
     'method': 'GET',
-    'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
+    // 'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
+    'url':"http://applis.univ-nc.nc/cgi-bin/WebObjects/ServeurPlanning.woa/wa/iCalendarOccupations?login=mlorella"
    
   };
-  options.url += 'cmarin03';
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.json(JSON.parse(response.body))
+
+
+  const request = http.get(options.url, function(response) {
+  response.pipe(file);
+
+  fs.readFile(file.path, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    let icalData = data;
+
+    let output = ical2json.convert(icalData);
+    console.log(output)
+    res.json(output)
+  })
+
   });
 });
 
 
 router.get("/:uid", (req, res) => {
+
+  var file = fs.createWriteStream(`${req.params.uid}.ics`);
   var options = {
     'method': 'GET',
-    'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
-   
+    // 'url': 'https://ical-to-json.herokuapp.com/convert.json?url=http%3A%2F%2Fapplis.univ-nc.nc%2Fcgi-bin%2FWebObjects%2FServeurPlanning.woa%2Fwa%2FiCalendarOccupations%3Flogin%3D',
+    'url':"http://applis.univ-nc.nc/cgi-bin/WebObjects/ServeurPlanning.woa/wa/iCalendarOccupations?login="
+  
   };
+
   options.url += req.params.uid;
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.json(JSON.parse(response.body))
+
+  const request = http.get(options.url, function(response) {
+  response.pipe(file).on('finish', function() {
+
+
+    var icalData = fs.readFileSync(file.path, 'utf8');
+
+    let output = ical2json.convert(icalData);
+
+    res.json(output)
   });
+
+    
+  });
+
 });
+
 
 app.use(`/.netlify/functions/api`, router);
 
